5382ac3d18e4f0b8c0876ce43a51dee4d7f03e60
Brian Yang <brian.b.yang@gmail.com>
2021-07-11 22:44:35 -0700

    IntelliJ IDEA
    Setup, registered. The official documents sucks. Let me find a tutorial tomorrow.

---
+++ 50 - Coding/Python/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Python',
+    version='1.1',
+    packages=['analysis.reports'],
+    url='http://example.com',
+    license='',
+    author='Brian',
+    author_email='jetbrains@jetbrains.com',
+    description='jetbrains@jetbrains.com'
+)
